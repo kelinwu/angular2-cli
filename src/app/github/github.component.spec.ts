5c94767e6f3061879618f2594c0f43d9b2284146
kelinw <kelinwu@live.com>
2016-08-06 01:48:26 -0400

    initial commit

---
+++ src/app/github/github.component.spec.ts
@@ -0,0 +1,13 @@
+/* tslint:disable:no-unused-variable */
+
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { addProviders, async, inject } from '@angular/core/testing';
+import { GithubComponent } from './github.component';
+
+describe('Component: Github', () => {
+  it('should create an instance', () => {
+    let component = new GithubComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/github/github.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-github',
+  templateUrl: 'github.component.html',
+  styleUrls: ['github.component.css']
+})
+export class GithubComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
