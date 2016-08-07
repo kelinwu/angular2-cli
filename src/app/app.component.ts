94b5f70effd73da991252115c9ca10de65c78b87
kelinw <kelinwu@live.com>
2016-08-07 01:31:54 -0400

    github search code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { GithubComponent } from './github/github.component';
+import {GithubService} from './services/github.service';
+import {HTTP_PROVIDERS} from '@angular/http';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [GithubComponent]
+  directives: [GithubComponent],
+  providers:[HTTP_PROVIDERS,GithubService]
 })
 
 export class AppComponent {
--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GithubService} from '../services/github.service';
 
 @Component({
   moduleId: module.id,
@@ -7,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['github.component.css']
 })
 export class GithubComponent implements OnInit {
+  user:any; repos: any; username:any;
+  constructor(private _githubService:GithubService) { 
+    this._githubService.getUser().subscribe(user => {
+            this.user = user;
+            console.log(this.user);
+        });
 
-  constructor() { }
+        this._githubService.getRepos().subscribe(repos => {
+            this.repos = repos;
+            console.log(this.repos);
+        });
+  }
 
   ngOnInit() {
   }
+
+  search(){
+        this._githubService.updateUsername(this.username);
+        
+        this._githubService.getUser().subscribe(user => {
+            this.user = user;
+        });
+
+        this._githubService.getRepos().subscribe(repos => {
+            this.repos = repos;
+        });
+    }
 
 }
