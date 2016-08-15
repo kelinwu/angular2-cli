7db87155acc92beb50a66f54284f7f97bee7ea29
kelinw <kelinwu@live.com>
2016-08-15 12:00:33 -0400

    remove spec.ts, some updates

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -2,33 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import {GithubService} from '../services/github.service';
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-github',
-  templateUrl: 'github.component.html',
-  styleUrls: ['github.component.css']
+    moduleId: module.id,
+    selector: 'app-github',
+    templateUrl: 'github.component.html',
+    styleUrls: ['github.component.css']
 })
+
 export class GithubComponent implements OnInit {
-  user:any; repos: any; username:any;
-  constructor(private _githubService:GithubService) { 
-    this._githubService.getUser().subscribe(user => {
+    user: any;
+    repos: any;
+    username: any;
+    constructor(private _githubService: GithubService) {
+        this._githubService.getUser().subscribe(user => {
             this.user = user;
-            console.log(this.user);
         });
 
         this._githubService.getRepos().subscribe(repos => {
             this.repos = repos;
-            console.log(this.repos);
         });
-  }
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  search(){
+    search() {
         this._githubService.updateUsername(this.username);
-        
+
         this._githubService.getUser().subscribe(user => {
             this.user = user;
+            console.log(user);
         });
 
         this._githubService.getRepos().subscribe(repos => {
--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -4,25 +4,25 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GithubService {
-  private username = "kelinwu";
-  private client_id = '8b3dd034fbd63166c343';
+    private username = "kelinwu";
+    private client_id = '8b3dd034fbd63166c343';
     private client_secret = '1752ae61e7476fab8be22db25b4df9066fe8c08e';
 
-  constructor(private _http: Http) { 
-    console.log('Github service started...')
-  }
+    constructor(private _http: Http) {
+        console.log('Github service started...')
+    }
 
-  getUser() {
-        return this._http.get('https://api.github.com/users/' + this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
+    getUser() {
+        return this._http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
             .map(res => res.json());
     }
 
     getRepos() {
-        return this._http.get('https://api.github.com/users/' + this.username + '/repos'+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
+        return this._http.get('https://api.github.com/users/' + this.username + '/repos' + '?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
             .map(res => res.json());
     }
 
-    updateUsername(username:any){
+    updateUsername(username: any) {
         this.username = username;
     }
 
